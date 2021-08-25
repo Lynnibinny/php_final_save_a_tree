fd2b27cbfcb3a7bf02e5e57690f2bdd969109e89
Lynnibinny <72465633+Lynnibinny@users.noreply.github.com>
2021-08-25 20:51:25 +0200

    Create register_login.php

---
+++ register_login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="login">
+			<h1>Anmelden</h1>
+			<form action="Website_register.php" method="post">
+				
+					<input type="submit" value="Registrieren">
+			</form>
+			<form action="Website_login.php" method="post">
+					<input type="submit" value="Login">
+			</form>
+		</div>
+	</body>
+</html>
